b31a466f83d6b04ce3448abad9b270b27af56421
smithynb <smithynb@oregonstate.edu>
2024-12-03 13:22:12 -0800

    404 page

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+var fs = require('fs')
 var express = require('express')
 var port = process.env.PORT || 3000
 var app = express()
 app.use(express.static(__dirname))
+
 
 //index.html
 app.get("/", function(req, res, next) {
@@ -11,24 +13,26 @@ app.get("/", function(req, res, next) {
 
 //flappybird page
 app.get("/flappybird", function(req, res, next) {
-    console.log("Request received")
     res.status(200).sendFile(__dirname + "/flappyBird/flappyBird.html")
 })
 //snake page
 app.get("/snake", function(req, res, next) {
-    console.log("Request received")
     res.status(200).sendFile(__dirname + "/snake/snake.html")
 })
 //leaderboard page
 app.get("/leaderboard", function(req, res, next) {
-    console.log("Request received")
     res.status(200).sendFile(__dirname + "/leaderboard/leaderboard.html")
 })
 
+//submit score to leaderboard
+app.post("/leaderboard/:gameId/addScore", function(req, res, next) {
+    console.log("addScore request received")
+    
+})
 
-//page not found, fallback
+//page not found
 app.get("*", function(req, res, next) {
-    res.status(404).send("404 Not Found (this is a placeholder for a real 404 html page)")
+    res.status(404).sendFile(__dirname + "/404.html")
 })
 
 app.listen(port, function(err){
