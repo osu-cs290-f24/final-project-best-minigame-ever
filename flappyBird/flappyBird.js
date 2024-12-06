a8811308f6990d07ae4f710530549c40a98ee030
Leo Sallos <sallosl@oregonstate.edu>
2024-12-06 15:49:05 -0800

    pipes can now be rendered

--- flappyBird/flappyBird.js
+++ flappyBird/flappyBird.js
@@ -1,16 +1,27 @@
 // global variables
 var interval = 16;
 var running = false;
+var score = 2;
+var pipes = [];
+var pipeCount = 0;
 var scoreElement = document.getElementById("current-score");
 const defaultBird = {
     top: 150,
     speed: 3.5,
-    width: 75,
-    height: 55,
+    width: 55,
+    height: 35,
     jumpSpeed: -10,
     gravity: 0.5
 };
 var bird;
+
+var pipeTest = true;
+
+function clearPipes () {
+    var pipeElements = document.getElementsByClassName("pipe-container");
+    for (var i = 0; i < pipeElements.length; i++)
+        pipeElements[i].remove();
+}
 
 function renderStart(){
     // reset bird position
@@ -22,11 +33,12 @@ function renderStart(){
         jumpSpeed: defaultBird.jumpSpeed,
         gravity: defaultBird.gravity
     };
-    console.log(defaultBird.top + ' ' + bird.top);
 
-    // wipe clean
-    
-    // render background, bird, starting pipes
+    // reset score
+    score = 0;
+
+    // remove all pipes
+    clearPipes();
 }
 
 // submit game score to leaderboard
@@ -83,6 +95,7 @@ function resumeGame(){
     hideGamePrompts();
 }
 
+// end game
 function endGame(){
     running = false;
 
@@ -94,13 +107,83 @@ function endGame(){
     losePrompt.classList.remove("hidden");
 
     // change score element
+    document.getElementById("lose-screen-score").textContent = "Score: " + score;
 }
 
 // update position of the bird
 function updateBird(){
     var gameBird = document.getElementById("bird-container");
     gameBird.style.top = bird.top + 'px';
-    // add future rotation
+    /*
+        * add bird rotation
+    */
+}
+
+// add new pipe
+function createPipe(){
+    var newPipe = {
+        left: 800,
+        topPipeBottom: Math.floor(Math.random() * 360) + 40
+    };
+    newPipe.bottomPipeTop = 400 - newPipe.topPipeBottom + 60;
+
+    pipes.push(newPipe);
+}
+
+// create new pipe element, add to DOM
+function createPipeElement(index){
+    // pipe container
+    var newPipeContainer = document.createElement('div');
+    newPipeContainer.classList.add("pipe-container");
+    newPipeContainer.id = "pipe-" + pipeCount;
+    newPipeContainer.style.left = pipes[index].left + "px";
+
+    // upper pipe
+    var upperPipe = document.createElement('div');
+    upperPipe.classList.add("upper-pipe");
+    upperPipe.style.bottom = pipes[index].topPipeBottom + "px";
+    var upperPipeImage = document.createElement('img');
+    upperPipeImage.classList.add("upper-pipe-img");
+    upperPipeImage.src = "pipe.png";
+    upperPipeImage.alt = "upper-pipe";
+    upperPipe.appendChild(upperPipeImage);
+    newPipeContainer.appendChild(upperPipe);
+
+    // lower pipe
+    var lowerPipe = document.createElement('div');
+    lowerPipe.classList.add("lower-pipe");
+    lowerPipe.style.top = pipes[index].bottomPipeTop + "px";
+    var lowerPipeImage = document.createElement('img');
+    lowerPipeImage.classList.add("lower-pipe-img");
+    lowerPipeImage.src = "pipe.png";
+    lowerPipeImage.alt = "upper-pipe";
+    lowerPipe.appendChild(lowerPipeImage);
+    newPipeContainer.appendChild(lowerPipe);
+
+    // add to DOM
+    var gameContainer = document.getElementById("game-container");
+    gameContainer.appendChild(newPipeContainer);
+}
+
+
+// update all pipe positions
+function updatePipes(){
+    // get all pipes
+    var pipeElements = document.getElementsByClassName("pipe-container");
+
+    // add new pipe
+    if (pipeElements.length > pipes.length){
+        createPipeElement();
+    }
+
+    // move pipes
+    for (var i = 0; i < pipes.length - 1; i++)
+        pipes[i].left -= 2;
+
+    // update DOM
+    for (var i = 0; i < pipeElements.length; i++){
+        pipeElements[i].style.left -= 2;
+    }
 }
 
 // update game
@@ -108,11 +191,9 @@ function update(){
     // stop if not running
     if (!running) return;
 
-    console.log("yeeee");
-
     // update game
     updateBird();
-        // update score element
+    document.getElementById("current-score").textContent = score;
 
     // move bird
     bird.top += bird.speed;
@@ -125,15 +206,34 @@ function update(){
         bird.top = 0;
 
     // check bird at bottom
-    if (bird.top > (400 - bird.height))
+    if (bird.top > (400 - (bird.height - 5)))
         endGame();
+
+    // add new pipe if needed
+    if (pipeTest){
+        createPipe();
+        createPipeElement(pipes.length - 1);
+        pipeTest = false;
+    }
+
+    // update pipe position
+    updatePipes();
+
+    // check bird collision
+    /*
+        * TO DO
+    */
+    
+    // check if scored
+    /*
+        * TO DO
+    */
 }
 
 // on key down
 document.addEventListener("keydown", (key) => {
     // jump
     if (key.code === "ArrowUp" || key.key == 'w' || key.code === "Space"){
-        console.log("Jump");
         birdJump();
     }
 
