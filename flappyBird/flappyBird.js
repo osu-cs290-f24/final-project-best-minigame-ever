dcd3c2491610a8b1885dee74111f1723acfbcf47
Leo Sallos <sallosl@oregonstate.edu>
2024-12-05 21:50:21 -0800

    end screen & bird mechanics added

--- flappyBird/flappyBird.js
+++ flappyBird/flappyBird.js
@@ -1,10 +1,43 @@
 // global variables
+var interval = 16;
 var running = false;
+var scoreElement = document.getElementById("current-score");
+const defaultBird = {
+    top: 150,
+    speed: 3.5,
+    width: 75,
+    height: 55,
+    jumpSpeed: -10,
+    gravity: 0.5
+};
+var bird;
 
 function renderStart(){
+    // reset bird position
+    bird = {
+        top: defaultBird.top,
+        speed: defaultBird.speed,
+        width: defaultBird.width,
+        height: defaultBird.height,
+        jumpSpeed: defaultBird.jumpSpeed,
+        gravity: defaultBird.gravity
+    };
+    console.log(defaultBird.top + ' ' + bird.top);
+
     // wipe clean
     
     // render background, bird, starting pipes
+}
+
+// submit game score to leaderboard
+function submitScore(){
+    /*
+        * TO DO
+    */
+}
+
+function birdJump(){
+    bird.speed = bird.jumpSpeed;
 }
 
 // hide all game prompts in the game container
@@ -18,20 +51,19 @@ function hideGamePrompts(){
 // start game
 function startGame(){
     if (!running){
-        console.log("START");
         running = true;
 
         // hide all game prompts
         hideGamePrompts();
 
         // generate logic
+        renderStart();
     }
 }
 
 // pause game
 function stopGame(){
     if (running){
-        console.log("STOP");
         running = false;
 
         // reset prompt screens
@@ -51,11 +83,58 @@ function resumeGame(){
     hideGamePrompts();
 }
 
+function endGame(){
+    running = false;
+
+    // reset prompt screens
+    hideGamePrompts();
+
+    // show pause screen
+    var losePrompt = document.getElementById("lose-screen-container");
+    losePrompt.classList.remove("hidden");
+
+    // change score element
+}
+
+// update position of the bird
+function updateBird(){
+    var gameBird = document.getElementById("bird-container");
+    gameBird.style.top = bird.top + 'px';
+    // add future rotation
+}
+
+// update game
+function update(){
+    // stop if not running
+    if (!running) return;
+
+    console.log("yeeee");
+
+    // update game
+    updateBird();
+        // update score element
+
+    // move bird
+    bird.top += bird.speed;
+
+    // update bird speed
+    bird.speed += bird.gravity;
+
+    // check bird at top
+    if (bird.top < 0)
+        bird.top = 0;
+
+    // check bird at bottom
+    if (bird.top > (400 - bird.height))
+        endGame();
+}
+
 // on key down
 document.addEventListener("keydown", (key) => {
     // jump
     if (key.code === "ArrowUp" || key.key == 'w' || key.code === "Space"){
         console.log("Jump");
+        birdJump();
     }
 
     // stop game
@@ -66,12 +145,15 @@ document.addEventListener("keydown", (key) => {
 var startButton = document.getElementById("start-game-button");
 var stopButton = document.getElementById("stop-game-button");
 var resumeButton = document.getElementById("resume-game-button");
+var restartButtons = document.getElementsByClassName("restart-game-button");
+var submitScoreButton = document.getElementById("submit-score-button");
+
 startButton.addEventListener("click", startGame);
 stopButton.addEventListener("click", stopGame);
 resumeButton.addEventListener("click", resumeGame);
+for (var i = 0; i < restartButtons.length; i++)
+    restartButtons[i].addEventListener("click", startGame);
+submitScoreButton.addEventListener("click", submitScore);
 
-
-
-// update game
-while (running){
-}
+// update every interval ms
+setInterval(update, interval);
